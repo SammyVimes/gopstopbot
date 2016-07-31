c20c188dc5c47609737275d6d9cedae850687445
alexeyev <anton.m.alexeyev@gmail.com>
2016-07-31 05:35:07 +0300

    uppercasing word stress commented

--- src/main/java/ru/gopstop/bot/engine/search/preprocessing/BasicPreprocessor.java
+++ src/main/java/ru/gopstop/bot/engine/search/preprocessing/BasicPreprocessor.java
@@ -1,5 +1,12 @@
 package ru.gopstop.bot.engine.search.preprocessing;
 
+import com.google.common.base.Joiner;
+import ru.gopstop.bot.engine.ExtraWordStressTool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by aam on 31.07.16.
  */
@@ -7,10 +14,23 @@ public class BasicPreprocessor {
 
     public static String postfix(String line) {
 
-        String normalLine = line.trim().replaceAll("[^a-zA-Zа-яА-я ]", "").toLowerCase();
+        String normalLine = line.trim().replaceAll("[^a-zA-Zа-яА-я ]", " ").toLowerCase();
+
+//        String[] splitted = normalLine.replaceAll("\\s+", " ").split(" ");
+
+//        String lastStressed = ExtraWordStressTool.upperCaseStress(splitted[splitted.length - 1]);
+//
+//        final List<String> res = new ArrayList<>(Arrays.asList(splitted));
+//        res.remove(splitted.length - 1);
+//
+//        res.add(lastStressed);
+//
+//        String fixedline = Joiner.on(" ").join(res);
+
+        String fixedline = normalLine;
+
         String processedLine =
-                new StringBuilder(
-                        normalLine.toLowerCase()).reverse().toString();
+                new StringBuilder(fixedline).reverse().toString();
         //todo augmentation
         return processedLine;
     }
