1053decf4407a19f0c4b0febb0be2a5c269eba53
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-01 12:18:56 +0300

    Settings returned

---
+++ src/main/java/ru/gopstop/bot/telegram/controller/SettingsController.java
@@ -0,0 +1,46 @@
+package ru.gopstop.bot.telegram.controller;
+
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.api.objects.Message;
+import ru.gopstop.bot.telegram.Constants;
+import ru.gopstop.bot.telegram.TGBot;
+import ru.gopstop.bot.telegram.internal.Emoji;
+import ru.gopstop.bot.telegram.user.TGSession;
+
+/**
+ * Created by Semyon on 30.07.2016.
+ */
+public class SettingsController extends Controller {
+
+    private static final String ABOUT = Constants.ABOUT + " " + Emoji.INFORMATION_SOURCE.toString();
+
+    public SettingsController(final TGBot bot) {
+        super(bot);
+    }
+
+    @Override
+    public String getKey() {
+        return Constants.ABOUT;
+    }
+
+    @Override
+    public boolean rememberMe() {
+        return false;
+    }
+
+    @Override
+    public String getEntry() {
+        return ABOUT;
+    }
+
+    @Override
+    public void handleMessage(final Message request, final TGSession session) throws TelegramApiException {
+        final String text = request.getText();
+
+        if (getEntry().equals(text)) {
+            sendMessage(request.getChatId().toString(), Constants.BOT_SELF_DESCRIPTION);
+            return;
+        }
+    }
+
+}
