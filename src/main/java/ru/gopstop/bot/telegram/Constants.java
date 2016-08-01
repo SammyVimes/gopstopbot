c687cbd82a2ac298775148ee72405db5ddfc6688
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-01 14:18:21 +0300

    info about bot

--- src/main/java/ru/gopstop/bot/telegram/Constants.java
+++ src/main/java/ru/gopstop/bot/telegram/Constants.java
@@ -16,7 +16,8 @@ public final class Constants {
     public static final String BOT_SELF_DESCRIPTION =
             "\"Мне говорят, что Окна ТАСС моих стихов полезнее...\"\n"
                     + "Like, share, re-post or stay with us,\n"
-                    + "Ведь и шансон - поэзия.";
+                    + "Ведь и шансон - поэзия.\n\n"
+            + "[Что здесь происходит?](https://github.com/SammyVimes/gopstopbot/blob/master/README.md)";
 
     private Constants() {
 
