2473e46bc1888d44ac5ffc10ce01feb71c15911f
aam <aam@sofitlabs.ru>
2016-08-05 11:22:52 +0300

    Logging

--- src/main/java/ru/gopstop/bot/engine/stress/WordStressMap.java
+++ src/main/java/ru/gopstop/bot/engine/stress/WordStressMap.java
@@ -189,6 +189,7 @@ public final class WordStressMap {
                     final String attempt = sb.toString();
 
                     if (stressDict.containsKey(attempt)) {
+                        LOGGER.info("Yofication: " + word + " -> " + attempt);
                         return attempt;
                     }
 
