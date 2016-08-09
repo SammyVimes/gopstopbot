ea63572f9baa260e98987d3d00888304df2fbc09
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-09 21:36:59 +0300

    Minor fix

--- src/test/java/ru/gopstop/bot/engine/search/RhymesHypothesisTest.java
+++ src/test/java/ru/gopstop/bot/engine/search/RhymesHypothesisTest.java
@@ -26,66 +26,66 @@ public class RhymesHypothesisTest extends TestCase {
     @Ignore
     public void testSelfContainedHypo() throws IOException {
 
-        final Directory directory = new RAMDirectory();
-        final Tokenizer tokenizer = new LetterTokenizer();
-        final CustomAnalyzer customAnalyzer = new CustomAnalyzer(tokenizer, 2, 3);
-        final IndexWriterConfig conf = new IndexWriterConfig(customAnalyzer);
-        final IndexWriter writer = new IndexWriter(directory, conf);
-
-        int num = 0;
-
-        List<String> testLines =
-                SongsUtils.listSongsByDir("data/songs/")
-                        .limit(1000)
-                        .flatMap(s -> s.getLyrics().stream())
-                        .collect(Collectors.toList());
-
-        System.out.println(testLines.toString());
-
-        for (String line : testLines) {
-
-            String processedLine = BasicPreprocessor.postfix(line, true);
-
-            System.out.println(processedLine);
-
-            final Document doc = new Document();
-            doc.add(new TextField("text", processedLine.substring(0, Math.min(6, processedLine.length())), Field.Store.YES));
-            doc.add(new StringField("fulltext", line, Field.Store.YES));
-            doc.add(new IntField("id", num, Field.Store.YES));
-            writer.addDocument(doc);
-        }
-
-        writer.close();
-
-        final IndexReader ir = DirectoryReader.open(writer.getDirectory());
-        final IndexSearcher is = new IndexSearcher(ir);
-
-        final BooleanQuery q = new BooleanQuery();
-        final String request =
-                BasicPreprocessor.postfix("мы подошли из-за угла", true);
-
-        for (String token : customAnalyzer.handle(request)) {
-            System.out.print(token + " ");
-            q.add(new TermQuery(new Term("text", token)), BooleanClause.Occur.SHOULD);
-        }
-
-        System.out.println();
-
-        int pageSize = 10;
-
-        final TopDocs docs = is.search(q, pageSize);
-
-        System.out.println("REQUEST: [" + BasicPreprocessor.postfix(request, true) + "]");
-
-        for (int i = 0; i < Math.min(pageSize, docs.totalHits); i++) {
-            System.out.println();
-            System.out.println(is.doc(docs.scoreDocs[i].doc).get("fulltext"));
-            System.out.println(is.doc(docs.scoreDocs[i].doc).get("text"));
-            System.out.println(is.doc(docs.scoreDocs[i].doc).get("author"));
-            System.out.println(is.doc(docs.scoreDocs[i].doc).get("title"));
-            System.out.println(docs.scoreDocs[i].score);
-        }
-
-
+//        final Directory directory = new RAMDirectory();
+//        final Tokenizer tokenizer = new LetterTokenizer();
+//        final CustomAnalyzer customAnalyzer = new CustomAnalyzer(tokenizer, 2, 3);
+//        final IndexWriterConfig conf = new IndexWriterConfig(customAnalyzer);
+//        final IndexWriter writer = new IndexWriter(directory, conf);
+//
+//        int num = 0;
+//
+//        List<String> testLines =
+//                SongsUtils.listSongsByDir("data/songs/")
+//                        .limit(1000)
+//                        .flatMap(s -> s.getLyrics().stream())
+//                        .collect(Collectors.toList());
+//
+//        System.out.println(testLines.toString());
+//
+//        for (String line : testLines) {
+//
+//            String processedLine = BasicPreprocessor.postfix(line, true);
+//
+//            System.out.println(processedLine);
+//
+//            final Document doc = new Document();
+//            doc.add(new TextField("text", processedLine.substring(0, Math.min(6, processedLine.length())), Field.Store.YES));
+//            doc.add(new StringField("fulltext", line, Field.Store.YES));
+//            doc.add(new IntField("id", num, Field.Store.YES));
+//            writer.addDocument(doc);
+//        }
+//
+//        writer.close();
+//
+//        final IndexReader ir = DirectoryReader.open(writer.getDirectory());
+//        final IndexSearcher is = new IndexSearcher(ir);
+//
+//        final BooleanQuery q = new BooleanQuery();
+//        final String request =
+//                BasicPreprocessor.postfix("мы подошли из-за угла", true);
+//
+//        for (String token : customAnalyzer.handle(request)) {
+//            System.out.print(token + " ");
+//            q.add(new TermQuery(new Term("text", token)), BooleanClause.Occur.SHOULD);
+//        }
+//
+//        System.out.println();
+//
+//        int pageSize = 10;
+//
+//        final TopDocs docs = is.search(q, pageSize);
+//
+//        System.out.println("REQUEST: [" + BasicPreprocessor.postfix(request, true) + "]");
+//
+//        for (int i = 0; i < Math.min(pageSize, docs.totalHits); i++) {
+//            System.out.println();
+//            System.out.println(is.doc(docs.scoreDocs[i].doc).get("fulltext"));
+//            System.out.println(is.doc(docs.scoreDocs[i].doc).get("text"));
+//            System.out.println(is.doc(docs.scoreDocs[i].doc).get("author"));
+//            System.out.println(is.doc(docs.scoreDocs[i].doc).get("title"));
+//            System.out.println(docs.scoreDocs[i].score);
+//        }
+//
+//
     }
 }
