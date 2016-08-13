b75ead154fca4397abab34a59a1bbc7b6b5255b8
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-13 16:33:38 +0300

    UncauchgExceptions

--- src/main/java/ru/gopstop/bot/GopStop.java
+++ src/main/java/ru/gopstop/bot/GopStop.java
@@ -5,9 +5,14 @@ import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.TelegramBotsApi;
 import ru.gopstop.bot.cache.SessionCache;
-import ru.gopstop.bot.engine.stress.WordStressMap;
 import ru.gopstop.bot.engine.search.LinesIndexer;
+import ru.gopstop.bot.engine.stress.WordStressMap;
 import ru.gopstop.bot.telegram.TGBot;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by Semyon on 30.07.2016.
@@ -41,7 +46,34 @@ public final class GopStop {
         LOGGER.trace("Есть чо?");
     }
 
+    static class WeirdExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(final Thread arg0, final Throwable arg1) {
+            System.out.println("[DEFAULT EXCEPTION HANDLER] Caught some exception " + arg0 + " " + arg1 + " " + arg1.getMessage());
+            System.err.println("[DEFAULT EXCEPTION HANDLER] Caught some exception " + arg0 + " " + arg1 + " " + arg1.getMessage());
+            arg1.printStackTrace();
+            arg1.printStackTrace(System.err);
+            System.exit(-1);
+        }
+    }
+
     public static void main(final String[] args) {
+
+        try {
+            final File fileErr = new File("err.log");
+            final PrintStream printStreamErr = new PrintStream(new FileOutputStream(fileErr));
+            System.setErr(printStreamErr);
+
+
+            final File file = new File("out.log");
+            final PrintStream printStream = new PrintStream(new FileOutputStream(file));
+            System.setErr(printStream);
+
+        } catch (final FileNotFoundException fnfe) {
+            LOGGER.error("Can't reseet System printstreams", fnfe);
+        }
+
+        Thread.setDefaultUncaughtExceptionHandler(new WeirdExceptionHandler());
         new GopStop().start();
     }
 
