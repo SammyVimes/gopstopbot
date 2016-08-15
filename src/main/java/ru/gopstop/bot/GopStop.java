4b90957dc479920f082c60803e5b0c5f626ea0e5
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-16 00:30:56 +0300

    Немного прокрастинации

--- src/main/java/ru/gopstop/bot/GopStop.java
+++ src/main/java/ru/gopstop/bot/GopStop.java
@@ -9,11 +9,6 @@ import ru.gopstop.bot.engine.search.LinesIndexer;
 import ru.gopstop.bot.engine.stress.WordStressMap;
 import ru.gopstop.bot.telegram.TGBot;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-
 /**
  * Created by Semyon on 30.07.2016.
  */
@@ -46,7 +41,8 @@ public final class GopStop {
         LOGGER.trace("Есть чо?");
     }
 
-    static class WeirdExceptionHandler implements Thread.UncaughtExceptionHandler {
+    private static class WeirdExceptionHandler implements Thread.UncaughtExceptionHandler {
+
         @Override
         public void uncaughtException(final Thread arg0, final Throwable arg1) {
             System.out.println("[DEFAULT EXCEPTION HANDLER] Caught some exception " + arg0 + " " + arg1 + " " + arg1.getMessage());
@@ -58,23 +54,6 @@ public final class GopStop {
     }
 
     public static void main(final String[] args) {
-
-        try {
-            final File fileErr = new File("err.log");
-            final PrintStream printStreamErr = new PrintStream(new FileOutputStream(fileErr));
-            System.setErr(printStreamErr);
-
-
-            final File file = new File("out.log");
-            final PrintStream printStream = new PrintStream(new FileOutputStream(file));
-            System.setErr(printStream);
-            LOGGER.info("Streams reset");
-
-        } catch (final FileNotFoundException fnfe) {
-            LOGGER.error("Can't reseet System printstreams", fnfe);
-            fnfe.printStackTrace();
-            fnfe.printStackTrace(System.err);
-        }
 
         Thread.setDefaultUncaughtExceptionHandler(new WeirdExceptionHandler());
         new GopStop().start();
